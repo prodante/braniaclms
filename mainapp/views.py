b154e905b3d18a0c0d0c2b1448e10101b699ab62
prodante <ya.dante8@yandex.ru>
2023-02-05 13:54:10 +0300

    lesson_3 Templates including

--- mainapp/views.py
+++ mainapp/views.py
@@ -13,10 +13,9 @@ class NewsPageView(TemplateView):
         context = super().get_context_data(**kwargs)
         # Create your own data
         context["news_title"] = "Громкий новостной заголовок"
-        context[
-            "news_preview"
-        ] = "Предварительное описание, которое заинтересует каждого"
+        context["news_preview"] = "Предварительное описание, которое заинтересует каждого"
         return context
+
 
 class CoursesPageView(TemplateView):
     template_name = "mainapp/courses_list.html"
